d15b5d197734ec89cfc28050595feed8ac54c020
LukasBenea <l.benea@students.htl-leonding.ac.at>
2026-04-07 15:48:41 +0200

    Site-setup

---
+++ project/php/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php
+        echo '<link    href="../css/index.css'   . '?' . time() . '" rel="stylesheet">';
+        echo '<script  src="../js/script.js'    . '?' . time() . '" defer></script>';
+    ?>
+</head>
+<body>
+    <div id="Wrapper">
+        <div id="pageBox">
+            <img id="skyline" src="../images/skyline.png" alt="skyline">
+        </div>
+    </div>
+    
+</body>
+</html>
